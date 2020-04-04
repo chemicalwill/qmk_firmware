001e39a74bc27995cee2fcc23d8e9d3c2d26ea47
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2020-04-04 08:44:31 -0500

    Returned ENTTG1 macro to _META
    Better for entering numbers from numpad layer

--- keyboards/cannonkeys/ortho48/keymaps/chemicalwill/keymap.c
+++ keyboards/cannonkeys/ortho48/keymaps/chemicalwill/keymap.c
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_META] = LAYOUT_ortho_4x12(
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,  KC_8,    KC_9,     MINS_SLSH,  KC_BSPC,
     KC_DEL,  _______, _______, _______, _______, _______, _______, KC_4,  KC_5,    KC_6,     PLUS_ASTR,  KC_DEL,
-    _______, _______, _______, _______, _______, _______, _______, KC_1,  KC_2,    KC_3,     KC_UP,      _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_1,  KC_2,    KC_3,     KC_UP,      ENTTG1,
     _______, TG_META, _______, _______, TG_META, _______, _______, KC_0,  KC_DOT,  KC_LEFT,  KC_DOWN,    KC_RIGHT
   ),
 
