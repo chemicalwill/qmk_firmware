513b1ac6f2830498cfadb25835cef4293c62fd57
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2020-08-03 11:47:15 -0500

    [ortho48] fixed _META tap dance
    * hold for MO(_META)
    * double hold for TG(_META) on

--- keyboards/cannonkeys/ortho48/keymaps/chemicalwill/work_tap_dances.c
+++ keyboards/cannonkeys/ortho48/keymaps/chemicalwill/work_tap_dances.c
@@ -907,6 +907,9 @@ void wk_finished (qk_tap_dance_state_t *state, void *user_data) {
                 layer_off(_META);
             } else {
                 layer_on(_META);
+        case DOUBLE_HOLD:
+            layer_on(_META);
+            break;
         }
         break;
     }
