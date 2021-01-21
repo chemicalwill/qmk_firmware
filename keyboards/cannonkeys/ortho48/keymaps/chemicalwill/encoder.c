8befe6f95da13d71d2da5f2cd81706ff74d77354
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2021-01-21 00:57:46 -0600

    [ortho48] moved encoder tab scroll to FN layer

--- keyboards/cannonkeys/ortho48/keymaps/chemicalwill/encoder.c
+++ keyboards/cannonkeys/ortho48/keymaps/chemicalwill/encoder.c
@@ -1,6 +1,6 @@
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (layer_state_is(_RAISE)) {
+    if (layer_state_is(_FN)) {
         if (clockwise) {
             tap_code16(C(KC_TAB));
         } else {
